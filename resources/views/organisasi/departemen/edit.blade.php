90f93a52bf4af97dcb09a80b96f6d2af3a7983c2
iyan1205 <mriyan078@gmail.com>
2024-03-20 16:06:03 +0700

    feature: add login

--- resources/views/organisasi/departemen/edit.blade.php
+++ resources/views/organisasi/departemen/edit.blade.php
@@ -28,7 +28,7 @@
                         <!-- left column -->
                         <div class="col-md-6">
                             <!-- general form elements -->
-                            <div class="card card-primary">
+                            <div class="card card-success">
                                 <div class="card-header">
                                     <h3 class="card-title">Form Edit Departemen</h3>
                                 </div>
