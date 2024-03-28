664941ce5680c22d3b72429f59f90d7d352d831b
Iyan Hermawan <mriyan078@gmail.com>
2024-03-28 16:01:12 +0700

    fix add karyawan

--- app/Http/Controllers/KaryawanController.php
+++ app/Http/Controllers/KaryawanController.php
@@ -81,17 +81,21 @@ class KaryawanController extends Controller
             // Tambahkan aturan validasi sesuai kebutuhan
         ]);
 
-            // Jika validasi gagal, kembali ke halaman sebelumnya dengan pesan kesalahan
+        // Jika validasi gagal, kembali ke halaman sebelumnya dengan pesan kesalahan
         if ($validator->fails()) {
-            return redirect()->back()->withInput()->withErrors($validator);
+            // Mengecek tab yang aktif dan mengarahkan kembali sesuai dengan tab yang aktif
+            if ($request->input('active_tab') == 'karyawan') {
+                return redirect()->back()->withInput()->withErrors($validator)->with('active_tab', 'karyawan');
+            } elseif ($request->input('active_tab') == 'pendidikan') {
+                return redirect()->back()->withInput()->withErrors($validator)->with('active_tab', 'pendidikan');
+            }
         }
-
-        // Pastikan NIK tidak ada dalam database sebelumnya
-        $existingKaryawan = Karyawan::where('nik', $request->input('nik'))->first();
-        if ($existingKaryawan) {
-            $validator->errors()->add('nik', 'NIK sudah ada dalam database.');
-            return redirect()->back()->withInput()->withErrors($validator);
-        }
+         // Pastikan NIK tidak ada dalam database sebelumnya
+         $existingKaryawan = Karyawan::where('nik', $request->input('nik'))->first();
+         if ($existingKaryawan) {
+             $validator->errors()->add('nik', 'NIK sudah ada dalam database.');
+             return redirect()->back()->withInput()->withErrors($validator);
+         }
 
         // Buat dan simpan karyawan baru
         $karyawan = Karyawan::create([
@@ -133,7 +137,7 @@ class KaryawanController extends Controller
         Session::flash('successAdd', $message);
 
         // Redirect ke halaman tertentu atau tampilkan pesan sukses
-        return redirect()->route('karyawan');
+        return redirect()->route('karyawan', $karyawan->id);
     }
 
 
@@ -184,87 +188,66 @@ class KaryawanController extends Controller
     }
 
     public function update(Request $request, $id)
-{
-    // Validasi input dari formulir
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|string|max:255',
-        'nik' => 'required|unique:karyawans,nik,'.$id,
-        'user_id' => 'nullable',
-        'departemen_id' => 'required',
-        'jabatan_id' => 'required',
-        'unit_id' => 'required',
-        'nomer_ktp' => 'nullable',
-        'tempat_lahir' => 'nullable',
-        'tanggal_lahir' => 'nullable',
-        'alamat_ktp' => 'nullable',
-        'gender' => 'nullable',
-        'institusi' => 'nullable',
-        'pendidikan_terakhir' => 'nullable',
-        'tahun_lulus' => 'nullable',
-        'status_ktp' => 'required',
-        'telepon' => 'required',
-        'npwp' => 'required',
-        'status_karyawan' => 'required', // kontrak_atau_tetap
-        'tgl_kontrak1' => 'required', // tglmasukdinas
-        'akhir_kontrak1' => 'required',
-        'status' => 'required', // aktif atau resign
-        'tgl_resign' => 'required',
-        'resign_id' => 'required',
+    {
+        // Validasi input dari form karyawan
+        $validatorKaryawan = Validator::make($request->all(), [
+            'user_id' => 'nullable',
+            'departemen_id' => 'nullable|exists:departemens,id',
+            'jabatan_id' => 'nullable|exists:jabatans,id',
+            'unit_id' => 'nullable|exists:units,id',
+            'name' => 'nullable|string|max:255',
+            'nik' => 'nullable|string|max:4',
+            'status_karyawan' => 'nullable|string|max:255',
+            'tgl_kontrak1' => 'nullable|string|max:255',
+            'akhir_kontrak1' => 'nullable|string|max:255',
+            'tgl_kontrak2' => 'nullable|string|max:255',
+            'akhir_kontrak2' => 'nullable|string|max:255',
+            'status' => 'nullable|string',
+            'resign' => 'nullable|string',
+            'resign_id' => 'nullable|exists:resign_reasons,id',
+            'nomer_ktp' => 'nullable|string|max:255',
+            'tempat_lahir' => 'nullable|string|max:255',
+            'tanggal_lahir' => 'nullable|date',
+            'gender' => 'nullable|date',
+            'alamat_ktp' => 'nullable|string',
+            'status_ktp' => 'nullable|string',
+            'telepon' => 'nullable|string',
+            'npwp' => 'nullable|string',
 
-        // Tambahkan aturan validasi sesuai kebutuhan
-    ]);
+        ]);
 
-    // Jika validasi gagal, kembali ke halaman sebelumnya dengan pesan kesalahan
-    if ($validator->fails()) {
-        return redirect()->back()->withInput()->withErrors($validator);
+        if ($validatorKaryawan->fails()) {
+            return redirect()->back()->withErrors($validatorKaryawan)->withInput();
+        }
+
+        // Update data karyawan
+        $karyawan = Karyawan::findOrFail($id);
+        $karyawan->update($validatorKaryawan->validated());
+
+        // Jika ada informasi pendidikan yang diubah, lakukan pembaruan juga
+        if ($karyawan->pendidikan) {
+            $karyawan->pendidikan->update([
+                'institusi' => $request->institusi,
+                'pendidikan_terakhir' => $request->pendidikan_terakhir,
+                'tahun_lulus' => $request->tahun_lulus
+                // Tambahkan pembaruan untuk field pendidikan lainnya jika diperlukan
+            ]);
+        } else {
+            // Jika informasi pendidikan tidak ada sebelumnya, tambahkan data baru
+            $karyawan->pendidikan()->create([
+                'institusi' => $request->institusi,
+                'pendidikan_terakhir' => $request->pendidikan_terakhir,
+                'tahun_lulus' => $request->tahun_lulus
+                // Tambahkan field pendidikan lainnya jika diperlukan
+            ]);
+        }
+        // Tambahkan session flash message
+        $message = 'Data Karyawan Berhasil Diedit';
+        Session::flash('successAdd', $message);
+
+        // Redirect ke halaman tertentu atau tampilkan pesan sukses
+        return redirect()->back();
     }
-
-    // Cari karyawan yang akan diperbarui
-    $karyawan = Karyawan::findOrFail($id);
-
-    // Perbarui data karyawan
-    $karyawan->update([
-        'name' => $request->input('name'),
-        'nik' => $request->input('nik'),
-        'nomer_ktp' => $request->input('nomer_ktp'),
-        'user_id' => $request->input('user_id'),
-        'departemen_id' => $request->input('departemen_id'),
-        'jabatan_id' => $request->input('jabatan_id'),
-        'unit_id' => $request->input('unit_id'),
-        'alamat_ktp' => $request->input('alamat_ktp'),
-        'tempat_lahir' => $request->input('tempat_lahir'),
-        'tanggal_lahir' => $request->input('tanggal_lahir'),
-        'gender' => $request->input('gender'),
-        'telepon' => $request->input('telepon'),
-        'status_ktp' => $request->input('status_ktp'),
-        'npwp' => $request->input('npwp'),
-        'tgl_kontrak1' => $request->input('tgl_kontrak1'),
-        'akhir_kontrak1' => $request->input('akhir_kontrak1'),
-        // Tambahkan kolom lain yang perlu disimpan
-    ]);
-
-    // Perbarui data pendidikan karyawan
-    $pendidikan = $karyawan->pendidikan;
-    $pendidikan->update([
-        'institusi' => $request->input('institusi'),
-        'pendidikan_terakhir' => $request->input('pendidikan_terakhir'),
-        'tahun_lulus' => $request->input('tahun_lulus'),
-        'nomer_ijazah' => $request->input('nomer_ijazah'),
-        'nomer_str' => $request->input('nomer_str'),
-        'exp_str' => $request->input('exp_str'),
-        'profesi' => $request->input('profesi'),
-        'cert_profesi' => $request->input('cert_profesi'),
-        //Tambahkan kolom lain di sini jika diperlukan
-    ]);
-
-    // Tambahkan pesan sukses ke session flash
-    $message = 'Data karyawan berhasil diperbarui';
-    Session::flash('successUpdate', $message);
-
-    // Redirect ke halaman tertentu atau tampilkan pesan sukses
-    return redirect()->route('karyawan');
-}
-
 
     /**
      * Remove the specified resource from storage.
