87db293ec6624b6779310cc88668eb9b539734d1
Iyan Hermawan <mriyan078@gmail.com>
2024-04-24 15:06:26 +0700

    fix: profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -28,7 +28,7 @@
                   <div class="card card-primary card-outline">
                     <div class="card-body box-profile">
                       <div class="text-center">
-                        <img class="profile-user-img img-fluid"
+                        <img class="profile-user-img"
                              src="{{ asset('storage/avatar/' . auth()->user()->image) }}"
                              alt="User profile picture">
                       </div>
